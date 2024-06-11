dc0f360b10d275020fb5d9b4e742fee0b271dd60
benhar-dev <53424421+benhar-dev@users.noreply.github.com>
2024-06-11 15:24:19 +1000

    initial commit

---
+++ src/hmi/CreatePrepareButton.js
@@ -0,0 +1,62 @@
+// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../Packages/Beckhoff.TwinCAT.HMI.Framework.12.760.59/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var hmi;
+        (function (hmi) {
+            function CreatePrepareButton(ObjectBrowser, Button) {
+
+                // here you can setup your button styles
+                const PREPARE_BACKGROUND_COLOR = { color: 'rgba(139, 26, 26, 1)' };
+                const DEFAULT_BACKGROUND_COLOR = null;
+
+                // Initialize a global map to hold observers keyed by element ID
+                if (!window.myObservers) {
+                    window.myObservers = {};
+                }
+
+                function setupObserver(divId) {
+                    const myDiv = document.getElementById(divId);
+
+                    if (!myDiv) {
+                        console.log("Element with ID '" + divId + "' not found.");
+                        return;
+                    }
+
+                    // Check if there is already an observer for this div and disconnect it
+                    if (window.myObservers[divId]) {
+                        window.myObservers[divId].disconnect();
+                    }
+
+                    // Function to execute when mutations are observed
+                    const callback = function (mutationsList, observer) {
+                        const hasClass = myDiv.querySelector('.TcHmi_Controls_Beckhoff_TcHmiTreeView-prepared-value') !== null;
+
+                        // here we apply styles based on hasClass.
+                        // add more here as needed.
+                        const button = Button.read();
+                        button.setBackgroundColor(hasClass ? PREPARE_BACKGROUND_COLOR : DEFAULT_BACKGROUND_COLOR);
+                    };
+
+                    // Create a new observer instance linked to the callback function
+                    window.myObservers[divId] = new MutationObserver(callback);
+
+                    // Start observing the target node and its descendants for configured mutations
+                    window.myObservers[divId].observe(myDiv, {
+                        attributes: true,
+                        attributeFilter: ['class'],
+                        subtree: true
+                    });
+                }
+
+                const objectBrowswerId = ObjectBrowser.read().getId();
+                setupObserver(objectBrowswerId);
+
+            }
+            hmi.CreatePrepareButton = CreatePrepareButton;
+        })(hmi = Functions.hmi || (Functions.hmi = {}));
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
+TcHmi.Functions.registerFunctionEx('CreatePrepareButton', 'TcHmi.Functions.hmi', TcHmi.Functions.hmi.CreatePrepareButton);
---
+++ src/hmi/Formatter.js
@@ -0,0 +1,26 @@
+// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../Packages/Beckhoff.TwinCAT.HMI.Framework.12.760.59/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var hmi;
+        (function (hmi) {
+            function Formatter(Input) {
+
+                console.log(Input);
+
+                const num = parseFloat(Input);
+                // Check if the input is a number and not NaN
+                if (!isNaN(num)) {
+                    // Return the number formatted to two decimal places
+                    return num.toFixed(2);
+                }
+
+                return Input;
+            }
+            hmi.Formatter = Formatter;
+        })(hmi = Functions.hmi || (Functions.hmi = {}));
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
+TcHmi.Functions.registerFunctionEx('Formatter', 'TcHmi.Functions.hmi', TcHmi.Functions.hmi.Formatter);
